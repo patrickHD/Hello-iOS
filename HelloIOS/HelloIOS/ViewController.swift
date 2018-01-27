03a17a867cda4e1dffcc3251fc0c78bfec1151f5
Patrick H Daniel Jr <patrickhdaniel@outlook.com>
2018-01-26 23:19:24 -0800

    Built UI + actions/outlet

--- HelloIOS/HelloIOS/ViewController.swift
+++ HelloIOS/HelloIOS/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func helloClicked(_ sender: UIButton) {
+    }
+    @IBAction func clearClicked(_ sender: UIButton) {
+    }
+    
 
 }
 
